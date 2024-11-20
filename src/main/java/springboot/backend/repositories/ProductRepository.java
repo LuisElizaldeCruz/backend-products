832576fbd243d4ec31a73de6cb2d04fba24fb5d7
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-11-19 21:32:47 -0600

    se agrego los cors al backend

--- src/main/java/springboot/backend/repositories/ProductRepository.java
+++ src/main/java/springboot/backend/repositories/ProductRepository.java
@@ -2,8 +2,10 @@ package springboot.backend.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springboot.backend.entities.Product;
 
+@CrossOrigin(origins = "http://127.0.0.1:5173")
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository <Product, Long> {
 }
